5d9e639f437c4394397926360a6e6911b3023500
hecto <hectorfx130@gmail.com>
2020-10-22 17:22:39 +0200

    gg

--- app/src/main/java/com/example/ajedrezchino/MainActivity.java
+++ app/src/main/java/com/example/ajedrezchino/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.ajedrezchino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.ajedrezchino.piezas.general;
 
@@ -42,8 +44,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-        tablero[4][1].setpieza(xgeneral);
-        ftablero[4][1] = (TextView) findViewById(R.id.E1);
+
+        tablero[4][0].setpieza(xgeneral);
+        ftablero[0][0] = (TextView) findViewById(R.id.A1);
+        ftablero[1][0] = (TextView) findViewById(R.id.B1);
+        ftablero[2][0] = (TextView) findViewById(R.id.C1);
+        ftablero[3][0] = (TextView) findViewById(R.id.D1);
+        ftablero[4][0] = (TextView) findViewById(R.id.E1);
+        ftablero[5][0] = (TextView) findViewById(R.id.F1);
+        ftablero[6][0] = (TextView) findViewById(R.id.G1);
+        ftablero[7][0] = (TextView) findViewById(R.id.H1);
+        ftablero[8][0] = (TextView) findViewById(R.id.I1);
+
+        ftablero[0][1] = (TextView) findViewById(R.id.A2);
+        ftablero[1][1] = (TextView) findViewById(R.id.B2);
+        ftablero[2][1] = (TextView) findViewById(R.id.C2);
+        ftablero[3][1] = (TextView) findViewById(R.id.D2);
+        ftablero[4][1] = (TextView) findViewById(R.id.E2);
+        ftablero[5][1] = (TextView) findViewById(R.id.F2);
+        ftablero[6][1] = (TextView) findViewById(R.id.G2);
+        ftablero[7][1] = (TextView) findViewById(R.id.H2);
+        ftablero[8][1] = (TextView) findViewById(R.id.I2);
+
+        ftablero[0][2] = (TextView) findViewById(R.id.A3);
+        ftablero[1][2] = (TextView) findViewById(R.id.B3);
+        ftablero[2][2] = (TextView) findViewById(R.id.C3);
+        ftablero[3][2] = (TextView) findViewById(R.id.D3);
+        ftablero[4][2] = (TextView) findViewById(R.id.E3);
+        ftablero[5][2] = (TextView) findViewById(R.id.F3);
+        ftablero[6][2] = (TextView) findViewById(R.id.G3);
+        ftablero[7][2] = (TextView) findViewById(R.id.H3);
+        ftablero[8][2] = (TextView) findViewById(R.id.I3);
+
+        ftablero[0][3] = (TextView) findViewById(R.id.A4);
+        ftablero[1][3] = (TextView) findViewById(R.id.B4);
+        ftablero[2][3] = (TextView) findViewById(R.id.C4);
+        ftablero[3][3] = (TextView) findViewById(R.id.D4);
+        ftablero[4][3] = (TextView) findViewById(R.id.E4);
+        ftablero[5][3] = (TextView) findViewById(R.id.F4);
+        ftablero[6][3] = (TextView) findViewById(R.id.G4);
+        ftablero[7][3] = (TextView) findViewById(R.id.H4);
+        ftablero[8][3] = (TextView) findViewById(R.id.I4);
+
+        ftablero[0][4] = (TextView) findViewById(R.id.A5);
+        ftablero[1][4] = (TextView) findViewById(R.id.B5);
+        ftablero[2][4] = (TextView) findViewById(R.id.C5);
+        ftablero[3][4] = (TextView) findViewById(R.id.D5);
+        ftablero[4][4] = (TextView) findViewById(R.id.E5);
+        ftablero[5][4] = (TextView) findViewById(R.id.F5);
+        ftablero[6][4] = (TextView) findViewById(R.id.G5);
+        ftablero[7][4] = (TextView) findViewById(R.id.H5);
+        ftablero[8][4] = (TextView) findViewById(R.id.I5);
+
         algoseleccionado = false;
         settablero();
 
@@ -59,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 if (tablero[i][j].getpieza() != null) {
                     if (p instanceof general) x = 0;
-                    else x = 6;
+                    else x = 1;
 
                     switch (x) {
                         case 0:
@@ -68,6 +120,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         default:
 
                     }
+                }
+                else{
+                    ftablero[i][j].setBackgroundResource(0);
                 }
             }
         }
@@ -364,7 +419,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         }
-
+        tablero[posicionclickada.getX()][posicionclickada.getY()].setpieza(tablero[ultimaposicion.getX()][ultimaposicion.getY()].getpieza());
+        tablero[ultimaposicion.getX()][ultimaposicion.getY()].setpieza(null);
+        ftablero[ultimaposicion.getX()][ultimaposicion.getY()].setBackgroundResource(0);
+        ultimaposicion = new Coordenadas(posicionclickada.getX(), posicionclickada.getY());
         settablero();
+
     }
+    //
 }
--- app/src/main/java/com/example/ajedrezchino/piezas/general.java
+++ app/src/main/java/com/example/ajedrezchino/piezas/general.java
@@ -7,6 +7,115 @@ import com.example.ajedrezchino.posicion;
 import java.util.ArrayList;
 
 
-public class general {
+public class general extends Pieza {
+
+    public general(boolean j) {
+        super(j);
+    }
+
+    @Override
+    public ArrayList<Coordenadas> movperm(Coordenadas coordenadas , posicion[][] tablero){
+        ArrayList<Coordenadas> movperm = new ArrayList<>();
+        movperm.clear();
+        Coordenadas c;
+
+        if((coordenadas.getX()+1) <9 && (coordenadas.getY()+1)<9){
+            if(tablero[coordenadas.getX()+1][coordenadas.getY()+1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY()+1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()+1][coordenadas.getY()+1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY()+1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getY()+1)<9){
+            if(tablero[coordenadas.getX()][coordenadas.getY()+1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX() , coordenadas.getY()+1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()][coordenadas.getY()+1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX() , coordenadas.getY()+1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getX()-1) >=0 && (coordenadas.getY()+1)<9){
+            if(tablero[coordenadas.getX()-1][coordenadas.getY()+1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY()+1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()-1][coordenadas.getY()+1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY()+1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getX()+1) <9 ){
+            if(tablero[coordenadas.getX()+1][coordenadas.getY()].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY());
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()+1][coordenadas.getY()].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY());
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getX()-1) <9 ){
+            if(tablero[coordenadas.getX()-1][coordenadas.getY()].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY());
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()-1][coordenadas.getY()].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY());
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getX()+1) <9 && (coordenadas.getY()-1)>=0){
+            if(tablero[coordenadas.getX()+1][coordenadas.getY()-1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY()-1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()+1][coordenadas.getY()-1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()+1 , coordenadas.getY()-1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getY()-1)>=0){
+            if(tablero[coordenadas.getX()][coordenadas.getY()-1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX() , coordenadas.getY()-1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()][coordenadas.getY()-1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX() , coordenadas.getY()-1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        if((coordenadas.getX()-1) <9 && (coordenadas.getY()-1)>=0){
+            if(tablero[coordenadas.getX()-1][coordenadas.getY()-1].getpieza() == null){
+                c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY()-1);
+                movperm.add(c);
+            }else{
+                if(tablero[coordenadas.getX()-1][coordenadas.getY()-1].getpieza().espieza() != tablero[coordenadas.getX()][coordenadas.getY()].getpieza().espieza()){
+                    c = new Coordenadas(coordenadas.getX()-1 , coordenadas.getY()-1);
+                    movperm.add(c);
+                }
+            }
+        }
+
+        return movperm;
+    }
 
 }
